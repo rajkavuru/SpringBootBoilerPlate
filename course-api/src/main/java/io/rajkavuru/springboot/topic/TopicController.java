19fff829da1655fd0bb0ae3caf036a4276cbd8a0
RajKavuru <raj.kavuru@gmail.com>
2018-04-05 12:33:41 -0700

    Initial version

---
+++ course-api/src/main/java/io/rajkavuru/springboot/topic/TopicController.java
@@ -0,0 +1,20 @@
+package io.rajkavuru.springboot.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+
+	@RequestMapping("/topics")
+	public List<Topic> getTopics(){
+		return Arrays.asList(
+				new Topic("raj", "AWS JAVA", "Cloud Architect"),
+				new Topic("anu", "Performance", "Performance Engineering"),
+				new Topic("xyz", "HP Tandem", "Non-Stop Servers")
+				);
+	}
+}
